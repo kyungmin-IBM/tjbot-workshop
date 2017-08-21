191509fcb645c69e5ccdd721b3515ff4595b0bbc
Kyungmin Lee <kyungmin.j.lee@gmail.com>
2017-08-20 22:02:52 -0400

    second

---
+++ examples/tjbot-speak/tjbot.js
@@ -0,0 +1,29 @@
+/**
+ * TJ Bot init js file
+ *
+ */
+
+// write your codes in main() method
+function main() {
+    console.log("TJBot speaking."); 
+    tj.speak("Hello, I am TJBot");
+}
+
+// IMPORTANT: DO NOT CHANGE THE CODE BELOW 
+const TJBot = require('tjbot');
+const config = require('/home/pi/Desktop/tjbot-workshop/config');
+
+// obtain our hardware configuration from config.js
+var hardware = config.hardware;
+
+// obtain our tjbot config from config.js
+var tjConfig = config.tjConfig;
+
+// obtain our credentials from config.js
+var credentials = config.credentials;
+
+var tj = new TJBot(hardware, tjConfig, credentials);
+
+main();
+
+console.log("TJBot has finished running. Goodbye!");
