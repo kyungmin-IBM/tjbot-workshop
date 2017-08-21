9d7a6f0210c0b04cc8fd01156f56734c54be8907
Kyungmin Lee <kyungmin.j.lee@gmail.com>
2017-08-21 00:00:57 -0400

    commit

---
+++ config.js
@@ -0,0 +1,53 @@
+// TJBOT HARDWARE
+exports.hardware = ['camera', 'led', 'microphone', 'servo', 'speaker'];
+
+// TJBOT CONFIG
+exports.tjConfig = {
+    log: {
+        level: 'verbose'
+    }
+};
+
+// WATSON CREDENTIALS
+exports.credentials = {};
+
+// Watson Conversation
+// https://www.ibm.com/watson/developercloud/conversation.html
+exports.credentials.conversation = {
+	password: 'YPriYS6wggKa',
+	username: 'd5e9573a-316c-471b-8434-7fd2aea12f7e'
+};
+
+// Watson Language Translator
+// https://www.ibm.com/watson/developercloud/language-translator.html
+exports.credentials.language_translator = {
+	password: 's5VdLdxcY8Zv',
+	username: '43e35dd6-952b-4383-955f-59c0243b6601'
+};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+	password: '0bbqzplRALtS',
+	username: '9e513087-9d9a-481b-ac22-0d82b7ca7166'
+};
+
+// Watson Text to Speech
+// https://www.ibm.com/watson/developercloud/text-to-speech.html
+exports.credentials.text_to_speech = {
+	password: 'ZJLuyruG4rKx',
+	username: 'e4b3f5e9-4c84-4ab4-9fd5-6f6622c243dd'
+};
+
+// Watson Tone Analyzer
+// https://www.ibm.com/watson/developercloud/tone-analyzer.html
+exports.credentials.tone_analyzer = {
+    password: '4aQn6OPjJLVq',
+    username: '3dbdcfd8-166d-4baa-9dc7-9d913b00cdf6'
+};
+
+// Watson Visual Recognition
+// https://www.ibm.com/watson/developercloud/visual-recognition.html
+exports.credentials.visual_recognition = {
+    api_key: '6bc5043bb1a6b443296d9838313d3509b099ce50'
+};
